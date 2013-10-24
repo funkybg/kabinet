c6f74134440b28e6a546a041b827925dbb542775
Martin Martev <mmartev@gmail.com>
2013-10-25 02:36:41 +0300

    offcanvas buton toggle

--- assets/js/app.js
+++ assets/js/app.js
@@ -43,8 +43,6 @@
 
     });
 
-
-
 //================= MENU SCROLL SCRIPTS ==================//
 
 $(window).scroll(function() {
@@ -84,6 +82,30 @@ $(function() {
         return false;
     });
 });
+
+//================= OFF CANVAS MENU TOGGLE SCRIPTS ==================//
+
+// $(function() {
+
+//     var items = $('.slideRight, .slideLeft');
+//     var content = $('.content');
+    
+//     var open = function() {
+//                             $(items).removeClass('close').addClass('open');
+//                         }
+//     var close = function() { 
+//                             $(items).removeClass('open').addClass('close');
+//                         }
+
+//     $('#navToggle').click(function(){
+//         if (content.hasClass('open')) {$(close)}
+//         else {$(open)}
+//     });
+//     content.click(function(){
+//         if (content.hasClass('open')) {$(close)}
+//     });
+
+// });
 
 //================= GOOGLE MAPS SCRIPTS ==================//
 
