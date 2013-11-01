73a16ee4473bf8cc65706d6e2cc0dc635af33226
Martin Martev <mmartev@gmail.com>
2013-11-01 22:54:42 +0200

    close menu mousedown/touchend

--- assets/js/app.js
+++ assets/js/app.js
@@ -92,7 +92,7 @@ $(function() {
     $('.content__mask--mobile, #map-escape').on('touchstart mousedown', function(){
         if (items.hasClass('open')) {$(close)}
     });
-    $('ul.offcanvas > li').click(function(){
+    $('ul.offcanvas > li').on('touchend mouseup', function(){
         if (items.hasClass('open')) {$(close)}
     });
 
