ebd5e1868c24889d6763b7f84f0d12a5cb9691a7
Martin Martev <mmartev@gmail.com>
2013-11-03 18:46:52 +0200

    cosmetics

--- assets/js/app.js
+++ assets/js/app.js
@@ -117,7 +117,7 @@ function initialize() {
         mapTypeId: google.maps.MapTypeId.ROADMAP
     }
 
-    var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+    var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
 
     var contentString = '<div id="content">'+
         '<div id="siteNotice">'+
